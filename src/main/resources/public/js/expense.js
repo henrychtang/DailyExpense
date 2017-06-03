8ed4a9a8995c4946f76da2790cfd2a05b1a354f5
henrychtang <henrychtang@gmail.com>
2017-06-03 15:23:09 +0800

    Update from iMac

--- src/main/resources/public/js/expense.js
+++ src/main/resources/public/js/expense.js
@@ -1,36 +1,61 @@
 angular.module('expenseApp', [])
-  .controller('expenseListController', function($scope, $http) {
-    var expenseList = this;
-    expenseList.expenses = [
-      {date:'2016-05-01', category:'Wellcome', amount:10},
-     {date:'2017-05-04', category:'Wet Market', amount:310}];
+  .controller('ExpenseController', function($scope, $http) {
+    var todoList = this;
 
-    expenseList.addExpense = function() {
-      expenseList.expenses.push({date:expenseList.date, category:expenseList.category, amount:expenseList.amount});
-      expenseList.date = '';
-      expenseList.category='';
-      expenseList.amount=0;
+//    todoList.expenses = [
+//          {date:'2017-05-30', category:'Wellcome', amount:50},
+//          {date:'2017-05-30', category:'WetMarket', amount:60}];
+
+
+    $http.get("expense/list")
+  .then(function(response) {
+      $scope.todoList.expenses = response.data;
+  });
+
+
+
+
+    $scope.addExpense = function(){
+		$scope.expense.push(newExpense);
+		// Writing it to the server
+		//
+		var dataObj = {
+				name : $scope.name,
+				employees : $scope.employees,
+				headoffice : $scope.headoffice
+		};
+		var res = $http.post('/savecompany_json', dataObj);
+		res.success(function(data, status, headers, config) {
+			$scope.message = data;
+		});
+		res.error(function(data, status, headers, config) {
+			alert( "failure message: " + JSON.stringify({data: data}));
+		});
+
+
+
+    todoList.todos = [
+      {text:'learn AngularJS', done:true},
+      {text:'build an AngularJS app', done:false}];
+
+    todoList.addTodo = function() {
+      todoList.todos.push({text:todoList.todoText, done:false});
+      todoList.todoText = '';
     };
 
-  //  expenseList.listExpense =function($scope, $http) {
-   //     $http.get("expense/list").then(function(response) {
-   //        $scope.expenses = response.data.records;
-        };
-    //};
+    todoList.remaining = function() {
+      var count = 0;
+      angular.forEach(todoList.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
 
-//    todoList.remaining = function() {
-//      var count = 0;
-//      angular.forEach(todoList.todos, function(todo) {
-//        count += todo.done ? 0 : 1;
-//      });
-//      return count;
-//    };
-//
-//    todoList.archive = function() {
-//      var oldTodos = todoList.todos;
-//      todoList.todos = [];
-//      angular.forEach(oldTodos, function(todo) {
-//        if (!todo.done) todoList.todos.push(todo);
-//      });
-//    };
+    todoList.archive = function() {
+      var oldTodos = todoList.todos;
+      todoList.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) todoList.todos.push(todo);
+      });
+    };
   });
